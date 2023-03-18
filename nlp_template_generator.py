5589ea6bf5820f85810b9fd65af90a03d8d235da
Saran Sankar <epsaransankar@gmail.com>
2023-03-18 16:30:32 -0400

    Create module for generating HTML templates from natural language prompts

---
+++ nlp_template_generator.py
@@ -0,0 +1,19 @@
+import os
+import openai
+
+# Load your API key from an environment variable or secret management service
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def get_templates(prompt, n):
+    response = openai.Completion.create(model="text-davinci-003", 
+    prompt=prompt, 
+    temperature=1,
+    n=n,
+    max_tokens=2000)
+    return response['choices']
+
+if __name__ == '__main__':
+    test_prompt = 'Generate an HTML template for an e-commerce site'
+    templates = get_templates(test_prompt, n=2)
+    for i in range(2):
+        print(templates[i]['text'])
